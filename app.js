d5b9967a0f16d3192b2bfe8f427308cd517f6ed1
saiballa <saiballa1301@gmail.com>
2024-09-21 20:06:59 +0530

    react learning

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-
+import React from "react"
+import ReactDOM from "react-dom/client";
 
 const heading  = React.createElement('div',{id:"parent"},
     [React.createElement('div',{id:"child"},
-        [React.createElement('h1',{},"h1 tag"),React.createElement('h1',{},"h2 tag")]),
+        [React.createElement('h1',{},"hello react"),React.createElement('h1',{},"h2 tag")]),
         React.createElement('div',{id:"child2"},
             [React.createElement('h1',{},"h1 tag"),React.createElement('h1',{},"h2 tag")])]);
 
